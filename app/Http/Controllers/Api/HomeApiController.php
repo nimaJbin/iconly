7ce53d2e00f21e6151c63691a12acb2ef3b0450a
nimaJbin <nimajbin@gmail.com>
2023-07-30 00:32:53 +0330

    swagger sanctum

--- app/Http/Controllers/Api/HomeApiController.php
+++ app/Http/Controllers/Api/HomeApiController.php
@@ -10,6 +10,20 @@ use App\Http\Services\Keys;
 class HomeApiController extends Controller
 {
 
+    /**
+     * @OA\Get(
+     ** path="/api/homepage",
+     *  tags={"Home Api"},
+     *     description="Showing list of icons with pagination",
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     **/
     public function homepage()
     {
         return response()->json([
@@ -21,7 +35,40 @@ class HomeApiController extends Controller
         ], 200);
     }
 
-
+    /**
+     * @OA\Post(
+     ** path="/api/export_icons",
+     *  tags={"Home Api"},
+     *  description="Export icons in zip file",
+     * @OA\RequestBody(
+     *    required=true,
+     * *         @OA\MediaType(
+     *           mediaType="multipart/form-data",
+     *           @OA\Schema(
+     *           @OA\Property(
+     *                  property="icons",
+     *                  description="Enter icons id's in Array => example [1,3,5,7]",
+     *                  type="string",
+     *       )
+     *     )
+     *   )
+     * ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Its Ok",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *           example ={
+     *               "result": true,
+     *               "message": "The zip file is ready.",
+     *               "data": {
+     *                   "Zip file path",
+     *              }
+     *        }
+     *      )
+     *   )
+     *)
+     **/
     public function exportIcons(Request $request)
     {
         return response()->json([
--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -198,6 +198,12 @@ return [
                     'in' => 'header', // The location of the API key. Valid values are "query" or "header".
                 ],
                 */
+                'sanctum' => [
+                    'type' => 'apiKey',
+                    'description' => 'Bearer <token>',
+                    'name' => 'Authorization',
+                    'in' => 'header',
+                ],
             ],
             'security' => [
                 /*
